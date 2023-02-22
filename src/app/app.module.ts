2780fdff5afb908e24ec753ed43878e82581f5f9
Italo-Alan <italoalan123@gmail.com>
2023-02-22 01:02:31 -0300

    :lipstick: feat: Parte do título feita, podendo ser feita por meio de Js, mas futuramente será visto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 
@@ -13,7 +14,8 @@ import { MainComponent } from './components/main/main.component';
     MainComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {trigger,state,style,animate,transition}from '@angular/animations';
+
+import { Input, Component } from '@angular/core';
+
+
+//Funciona, mas não é o que eu quero
+// const enterTransition = transition(':enter', [
+//   style({
+//     opacity: 0
+//   }),
+//   animate('1s ease-in', style({opacity: 1})),
+// ])
+// const leaveTransition = transition(':leave', [
+//   style({
+//     opacity: 1
+//   }),
+//   animate('1s ease-out', style({opacity: 0})),
+// ])
+// const fadeIn = trigger('fadeIn', [enterTransition])
+// const fadeOut = trigger('fadeOut', [leaveTransition])
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
 export class MainComponent {
-  firstParagraph: string = "Oi,";
-  secondParagraph: string = "Eu sou o Italo Alan";
-  thirdParagraph: string = "Desenvolvedor Front-End.";
-  descriptionParagraph: string = "Sou um desenvolvedor Front-End que está se aventurando no mundo do Back-End por meio do Java. ";
+  constructor(){
+
+  }
+
+  firstTitle: any = "Oi,";
+  secondTitle: string = "Eu sou o Italo Alan";
+  thirdTitle: string = "Desenvolvedor Front End.";
+  descriptionTitle: string = "Sou um desenvolvedor Front-End que está se aventurando em Javascript, Typescript e Angular na parte do Front. Aprendendo Java para usar no Back-End. Apaixonado pelo desenvolvimento Web. Cursando Análise e Desenvolvimento de Sistemas. ";
+
+
+  turnArray(text: string){
+    // console.log(this.firstTitle.split(''));
+    return text.split('');
+  };
+  turnArray2(text: string){
+    console.log(this.secondTitle.split(' ')[0]);
+    return text.split('')[0];
+  };
+
+  paragraphTest = this.turnArray2(this.firstTitle[0]);
+
+  paragraph1 = this.turnArray(this.firstTitle);
+  paragraph2 = this.turnArray(this.secondTitle);
+  paragraph3 = this.turnArray(this.thirdTitle);
+
 }
